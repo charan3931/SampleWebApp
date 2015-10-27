18d810bfbd86557698df8f390a321181b87a2311
ztarbug <kronprinzfriedrich@gmx.de>
2015-10-27 22:15:48 +0100

    add first content

---
+++ src/main/java/de/starwit/auth/SampleWebApp/App.java
@@ -0,0 +1,13 @@
+package de.starwit.auth.SampleWebApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
